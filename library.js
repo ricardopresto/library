456d37ba8ca350417e5a6c1dd4d7fc5aebf87b94
ricardopresto <ricardopresto5@gmail.com>
2019-02-27 20:12:06 +0100

    second commit

--- library.js
+++ library.js
@@ -6,7 +6,7 @@ let submit = document.getElementById("submit");
 let title = document.getElementById("title");
 let author = document.getElementById("author");
 let year = document.getElementById("year");
-let card = document.getElementById("card");
+let card = document.getElementById("card0");
 let container = document.getElementById("container");
 
 
@@ -24,9 +24,24 @@ addBtn.onclick = function() {
 submit.onclick = function() {
   let newbook = new book(title.value, author.value, year.value);
   library.push(newbook);
+  let index = library.length-1;
+  if (index == 0) {
+    let details = card.childNodes;
+    details[1].textContent = newbook.title;
+    details[3].textContent = newbook.author;
+    details[5].textContent = newbook.year;
+  }
+  else {
   let newcard = card.cloneNode(true);
   container.appendChild(newcard);
-  //alert(library[library.length-1].title);
-  
+  let details = newcard.childNodes;
+  details[1].textContent = newbook.title;
+  details[3].textContent = newbook.author;
+  details[5].textContent = newbook.year;
+  newcard.id = `card${index}`;
+  }
+  title.value = "";
+  author.value = "";
+  year.value = "";
   modal.style.display = "none";
 }
