cefcb29f0327c6f646f6ae2774afcdaee098ad46
ricardopresto <ricardopresto5@gmail.com>
2019-03-05 18:41:05 +0100

    added remove button

--- library.js
+++ library.js
@@ -8,7 +8,7 @@ let author = document.getElementById("author");
 let year = document.getElementById("year");
 let card = document.getElementById("card0");
 let container = document.getElementById("container");
-
+let remove = document.getElementById("rem0");
 
 function book(title, author, year) {
 	this.title = title;
@@ -26,22 +26,35 @@ submit.onclick = function() {
   library.push(newbook);
   let index = library.length-1;
   if (index == 0) {
-    let details = card.childNodes;
-    details[1].textContent = newbook.title;
-    details[3].textContent = newbook.author;
-    details[5].textContent = newbook.year;
+    let details = card.getElementsByTagName("*");
+    details[0].textContent = newbook.title;
+    details[1].textContent = newbook.author;
+    details[3].textContent = newbook.year;
+    details[4].addEventListener("click", removeFunction);
   }
   else {
-  let newcard = card.cloneNode(true);
-  container.appendChild(newcard);
-  let details = newcard.childNodes;
-  details[1].textContent = newbook.title;
-  details[3].textContent = newbook.author;
-  details[5].textContent = newbook.year;
-  newcard.id = `card${index}`;
+    let newcard = card.cloneNode(true);
+    let details = newcard.getElementsByTagName("*");
+    details[0].textContent = newbook.title;
+    details[1].textContent = newbook.author;
+    details[3].textContent = newbook.year;
+    details[4].id = `rem${index}`;
+    newcard.id = `card${index}`;
+    container.appendChild(newcard);
+    details[4].addEventListener("click", removeFunction);
   }
   title.value = "";
   author.value = "";
   year.value = "";
   modal.style.display = "none";
+}
+
+function removeFunction() {
+  let toRemove = String(event.srcElement.id).slice(3);
+  toRemove = Number(toRemove);
+  let rem = document.getElementById(`card${toRemove}`);
+  container.removeChild(rem);
+  //TO DO:
+  //also remove from library array
+  //check removal of card0
 }
